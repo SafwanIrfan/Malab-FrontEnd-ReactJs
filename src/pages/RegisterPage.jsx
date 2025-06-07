c269a4471785a5b0c83e91cda4b5bfa79acbed6f
SafwanIrfan <mohdsafwan2k5@gmail.com>
2025-06-08 02:19:59 +0500

    Few changes in register page

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -5,6 +5,7 @@ import axios from "axios";
 import { FaSpinner } from "react-icons/fa";
 import appLogo from "../assets/applogo.svg";
 import { FaEye, FaEyeSlash } from "react-icons/fa6";
+import { LockClosedIcon, MobileIcon, PersonIcon } from "@radix-ui/react-icons";
 
 const RegisterPage = () => {
    const { register } = useAuth();
@@ -20,6 +21,7 @@ const RegisterPage = () => {
    const [confirmPassword, setConfirmPassword] = useState("");
    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
    const [isError, setIsError] = useState(false);
+   const [phoneNo, setPhoneNo] = useState();
 
    useEffect(() => {
       if (googleLogin) {
@@ -83,41 +85,54 @@ const RegisterPage = () => {
 
    return (
       <div className="bg-white-color min-h-screen">
-         <div className="flex justify-center items-center my-10">
-            <div className="text-black w-96 p-6 rounded-lg bg-white shadow-2xl transition-all">
-               <h2 className="text-3xl font-black mb-4 text-center text-green-color">
-                  Register
+         <div className="grid grid-cols-1 md:grid-cols-2">
+            <div className=" text-black mx-10 p-6 rounded-lg bg-white-color shadow-md transition-all">
+               <h2 className="text-3xl font-bold text-green-color mb-1">
+                  Create an account
                </h2>
+               <p className="mb-4 text-gray-400 font-semibold ">
+                  Join us and play with ease!
+               </p>
 
                <form className="" onSubmit={handleSubmit}>
                   <div>
                      <label className="block mb-2 text-xl text-green-color">
-                        Username
+                        Full Name
                      </label>
-                     <input
-                        className="focus:outline-none bg-gray-300 focus:bg-gray-100 p-2 rounded w-full"
-                        type="text"
-                        name="username"
-                        placeholder="Username"
-                        onChange={handleChange}
-                        onFocus={() => setSameUsername(false)}
-                        required
-                     />
-                     {sameUsername && (
-                        <p className="text-red-600">Username already exist!</p>
-                     )}
+                     <div className="relative">
+                        <p className="absolute left-3 top-3">
+                           <PersonIcon className="w-5 h-5" />
+                        </p>
+                        <input
+                           className="pl-10 outline-none border-2 border-sgreen-color/40 focus:border-sgreen-color/80 duration-300 p-2 rounded-full transition-all w-full"
+                           type="text"
+                           name="username"
+                           placeholder="Enter your name"
+                           onChange={handleChange}
+                           onFocus={() => setSameUsername(false)}
+                           required
+                        />
+                        {sameUsername && (
+                           <p className="text-red-600">
+                              Username already exist!
+                           </p>
+                        )}
+                     </div>
                   </div>
 
                   <div className="mt-4">
                      <label className="block mb-2 text-xl text-green-color">
                         Password
                      </label>
-                     <div>
+                     <div className="relative">
+                        <p className="absolute left-3 top-3">
+                           <LockClosedIcon className="w-5 h-5" />
+                        </p>
                         <input
-                           className="focus:outline-none bg-gray-300 focus:bg-gray-100 p-2 rounded w-full"
+                           className="pl-10 outline-none border-2 border-sgreen-color/40 focus:border-sgreen-color/80 duration-300 p-2 rounded-full transition-all w-full"
                            type={showPassword ? "text" : "password"}
                            name="password"
-                           placeholder="Password"
+                           placeholder="Enter your password"
                            onChange={handleChange}
                            required
                         />
@@ -136,14 +151,17 @@ const RegisterPage = () => {
 
                   <div className="mt-4">
                      <label className="block mb-2 text-xl text-green-color">
-                        Confirm your Password
+                        Confirm Password
                      </label>
-                     <div>
+                     <div className="relative">
+                        <p className="absolute left-3 top-3">
+                           <LockClosedIcon className="w-5 h-5" />
+                        </p>
                         <input
-                           className="focus:outline-none bg-gray-300 focus:bg-gray-100 p-2 rounded w-full"
+                           className="pl-10 outline-none border-2 border-sgreen-color/40 focus:border-sgreen-color/80 duration-300 p-2 rounded-full transition-all w-full"
                            type={showConfirmPassword ? "text" : "password"}
                            name="password"
-                           placeholder="Password"
+                           placeholder="Confirm your password"
                            value={confirmPassword}
                            onChange={(e) => setConfirmPassword(e.target.value)}
                            onFocus={() => setIsError(false)}
@@ -164,6 +182,28 @@ const RegisterPage = () => {
                         {isError && (
                            <p className="text-red-500">Password is not same!</p>
                         )}
+                     </div>
+                  </div>
+
+                  <div>
+                     <div className="mt-4">
+                        <label className="block mb-2 text-xl text-green-color">
+                           Enter Phone No
+                        </label>
+                     </div>
+                     <div className="relative">
+                        <p className="absolute left-3 top-3">
+                           <MobileIcon className="w-5 h-5" />
+                        </p>
+                        <input
+                           className="pl-10 outline-none border-2 border-sgreen-color/40 focus:border-sgreen-color/80 duration-300 p-2 rounded-full transition-all w-full"
+                           type="number"
+                           name="phone"
+                           placeholder="+92"
+                           value={phoneNo}
+                           onChange={(e) => setPhoneNo(e.target.value)}
+                           required
+                        />
                      </div>
                   </div>
 
@@ -192,6 +232,26 @@ const RegisterPage = () => {
                <Link className="flex justify-center text-green-color" to="/">
                   <img className="w-20" src={appLogo} />
                </Link>
+            </div>
+            <div className="text-balance flex flex-col gap-4 py-10 items-center">
+               <img
+                  src={appLogo}
+                  className="h-32 w-32 rounded-full bg-white/90 p-2"
+               />
+               <p className="text-3xl font-semi-bold text-green-color text-center">
+                  Find the court and book it just by one click!
+               </p>
+               <div className="flex gap-2">
+                  <p className="bg-green-color px-4 py-1 rounded-full text-white">
+                     100+ Courts
+                  </p>
+                  <p className="bg-green-color px-4 py-1 rounded-full text-white">
+                     Secure Transactions
+                  </p>
+                  <p className="bg-green-color px-4 py-1 rounded-full text-white">
+                     5000+ Users
+                  </p>
+               </div>
             </div>
          </div>
       </div>
